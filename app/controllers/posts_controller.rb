25c6820be1983a60e56e43bf91623093060121cb
藤井真智子 <fujiimachiko@fujiimachikos-MacBook-Pro.local>
2021-04-20 12:45:10 +0900

    Created Controller and View

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def _form
+  end
+
+  def confirm
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'posts/_form'
+  get 'posts/confirm'
+  get 'posts/edit'
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
