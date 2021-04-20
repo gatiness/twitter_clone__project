71f5fd9aee696a005a9c18299ecb8cb644c10953
藤井真智子 <fujiimachiko@fujiimachikos-MacBook-Pro.local>
2021-04-20 16:03:49 +0900

    defined more controller actions in posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,57 @@
 class PostsController < ApplicationController
-  def _form
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
   end
 
-  def confirm
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path,notice: "Created the post！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
-  def index
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Edited the post！"
+    else
+      render :edit
+    end
   end
 
-  def new
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"Deleted the post！"
   end
 
-  def show
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
